e4645b5b6aece5bd0011cef66c30218043f98d95
chiosa <ionut.chiosa@softvision.ro>
2015-07-18 18:41:43 +0300

    fix nickname

--- a7lan7is_n3t/getGroups.php
+++ a7lan7is_n3t/getGroups.php
@@ -5,7 +5,7 @@ require(dirname(__FILE__) . '/index.php');
 $name = isset($_REQUEST['name']) ? $_REQUEST['name'] : null;
 $category = isset($_REQUEST['category']) ? $_REQUEST['category'] : null;
 $location = isset($_REQUEST['location']) ? $_REQUEST['location'] : null;
-$nickname = isset($_REQUEST['skypeid']) ? $_REQUEST['skypeid'] : null;
+$nickname = !empty($_REQUEST['skypeid']) ? $_REQUEST['skypeid'] : null;
 
 $groupModel = new Groups();
 
@@ -15,7 +15,7 @@ $name = strtolower($name);
 if (!empty($nickname) && !empty($name) && isset($groups[$name])) {
     //user join group
     $userModel = new User();
-    $userModel->joinGroup($name, $groups[$name]['group_name']);
+    $userModel->joinGroup($nickname, $groups[$name]['group_name']);
 }
 
 echo json_encode($groups);
